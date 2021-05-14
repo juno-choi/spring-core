faeacb066968d29581907f35199694485e357b8c
juno-choi <ililil9482@naver.com>
2021-05-14 21:04:05 +0900

    1. spring의 bean과 spring container 2. spring java setting과 spring java setting

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 - 간단한 회원과 상품에 대한 할인이 적용되는 사이트
 
 
+`blog 정리글`  https://ililil9482.tistory.com/122
+
 `출처` https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         //의존관계를 주입해주는 appConfig를 통해 구현체를 가져옴
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        /*AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
         //MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
