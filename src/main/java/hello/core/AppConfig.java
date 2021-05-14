f01e8076cf34f1acff6333481da26858a9625adf
juno-choi <ililil9482@naver.com>
2021-05-14 21:04:05 +0900

    1. spring의 bean과 spring container 2. spring java setting과 spring java setting

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //impl class (구현체)에서 구현체를 의존하지 않도록 AppConfig에서 주입해줌 == 의존관계 주입
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         //생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
